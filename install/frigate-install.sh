514a0d283c7919f5f85f72693c6ad3dceb6971f8
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-06-18 23:00:37 -0400

    Remove TensorRT v10 patch as it has been merged upstream

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -159,7 +159,7 @@ if [ $nvidia_installed == 1 ]; then
   echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> /etc/bash.bashrc
   ldconfig
   # Temporarily get my patched frigate tensorrt.py plugin (with support for TensorRT v10)
-  curl -s https://raw.githubusercontent.com/remz1337/frigate/dev/frigate/detectors/plugins/tensorrt.py > /opt/frigate/frigate/detectors/plugins/tensorrt.py
+  #curl -s https://raw.githubusercontent.com/remz1337/frigate/dev/frigate/detectors/plugins/tensorrt.py > /opt/frigate/frigate/detectors/plugins/tensorrt.py
   msg_ok "Installed TensorRT"
 
   msg_info "Installing TensorRT Object Detection Model (Resilience)"
